5dfafa5fe5520f3d3ad3a70eda7e6d8617f0d1c3
JGuzman87 <jguzman913@gmail.com>
2025-01-29 21:13:29 -0500

    started form logic

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,27 @@
-console.log("Hello")
+const userName = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
+const submitBtn = document.getElementById('submit');
+
+function storeBlog() {
+  const blogPost = {
+    username: userName.value,
+    title: title.value,
+    content: content.value,
+  };
+
+  localStorage.setItem('blogPost', JSON.stringify(blogPost));
+}
+
+function renderBlog() {
+
+}
+
+
+// localStorage.getItem()
+
+submitBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    storeBlog();
+    console.log('it works')
+})
