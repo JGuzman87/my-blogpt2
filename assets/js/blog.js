394ce880c8d4b16d5c93ddfb45118fa012876ccd
JGuzman87 <jguzman913@gmail.com>
2025-02-01 18:34:53 -0500

    fixed css for form page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function renderBlog() {
     const blog = JSON.parse(localStorage.getItem("blogPost"));
 
-    if (blog) {
+    if (blog !== null) {
       document.getElementById("saved-title").textContent = blog.title;
       document.getElementById("saved-username").textContent = blog.username;
       document.getElementById("saved-content").textContent = blog.content;
